2071755d9880033590f0461cd391949d2fb534ab
Anderson <77341608+ander-Sgr@users.noreply.github.com>
2024-08-09 11:27:00 +0000

    adding function  for delete and update

--- client.py
+++ client.py
@@ -8,13 +8,17 @@ URL = "http://127.0.0.1:8080/"
 
 def parser_args(args: List) -> Namespace:
     parser = ArgumentParser(description="API REST")
-    parser.add_argument('--insert', type=str, help="Data to insert at the db")
-    parser.add_argument('--getdata', action='store_true',help="Data to delete at the db")
+    parser.add_argument('--insert', type=str, help="Data to insert on the db")
+    parser.add_argument('--getdata', action='store_true',help="Recall all the data from the db")
+    parser.add_argument('--delete', type=str, help="Data to delete on the db")
+    parser.add_argument('--update', action='store_true', help="action for update a value")
+    parser.add_argument('--old_data', type=str, help="old data to replace")
+    parser.add_argument('--new_data',type=str, help='new data for replace the old data')
     return parser.parse_args(args)
 
 def insert_data(data: str):
     params = {'data': data}
-    response = requests.put(f'{URL}insert_data', params=params)
+    response = requests.post(f'{URL}insert_data', params=params)
     print(f"Response status code: {response.status_code}")
     print(f"Response content: {response.text}")
 
@@ -24,13 +28,40 @@ def get_data():
         print(f"Response content: {response.text}")
     elif response.status_code == 400:
         print("Resource not found")
-        
+
+def delete_data(data: str):
+    params = {'data': data}
+    response = requests.delete(f"{URL}delete", params=params)
+    if response.ok:
+        print(f"Response content: {response.text}")
+    else:
+        print(f"Response status ccode: {response.status_code}")
+        print(f"Response content: {response.content}")
+
+def update_data(data: str, new_data: str):
+    params = {'data': data, 'new_data': new_data}
+    response = requests.put(f"{URL}update", params=params)
+    if response.ok:
+        print(f"Response content: {response.content}")
+    else:
+        print(f"Response status ccode: {response.status_code}")
+        print(f"Response content: {response.content}")
+
 def main(args: list):
     parsed_args = parser_args(args)
     if parsed_args.insert:
         insert_data(parsed_args.insert)
-    elif  parsed_args.getdata:
+    elif parsed_args.getdata:
         get_data()
+    elif parsed_args.delete:
+        delete_data(parsed_args.delete)
+    elif parsed_args.update:
+        if not parsed_args.old_data:
+            print("No data provided")
+        if not parsed_args.new_data:
+            print("No value to update provided")
+        update_data(parsed_args.old_data, parsed_args.new_data)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, Response
 import csv
 import os
 from typing import List
@@ -6,37 +6,47 @@ from typing import List
 app = Flask(__name__)
 CSV_FILE = 'data.csv'
 
-def create_csv_file(file_name=CSV_FILE):
+def create_csv_file(file_name: str=CSV_FILE) -> None:
     if not os.path.isfile(file_name):
         with open(file_name, mode="w", newline="") as file:
           csv.writer(file)
 
-def write_data(data, file_name=CSV_FILE):
+
+def write_data(data: str, file_name: str =CSV_FILE)-> None:
    create_csv_file(file_name)
    with open(file_name, mode="a", newline="") as file:
       writer = csv.writer(file)
       writer.writerow([data])
+
 
 def read_data() -> str:
    if not os.path.isfile(CSV_FILE):
       return ""
    with open(CSV_FILE, mode="r") as file:
       return file.read()
-   
-def data_exists(data: str) -> bool: 
-   lines: List[str]
 
+
+def data_exists(data: str) -> bool: 
    lines = read_data().splitlines()
    for line in lines:
       if line == data:
          return True
    return False
 
-def replace_data(data):
-   s
-   
 
-@app.route("/insert_data", methods=['PUT'])
+def replace_data(data: str, new_data: str, file_name: str = CSV_FILE) -> None:
+   lines = read_data().splitlines()
+   updated_lines = []
+
+   for line in lines:
+      update_line = line.replace(data, new_data).strip()
+      if update_line:
+         updated_lines.append(update_line)
+   with open(file_name, mode='w') as file:
+      file.write("\n".join(updated_lines) + "\n")
+
+
+@app.route("/insert_data", methods=['POST'])
 def insert_data():
    value = request.args.get('data')
    if not value:
@@ -61,9 +71,20 @@ def delete_data():
       return Response("No data provided", status=400, mimetype="text/plain")
    if not data_exists(value):
       return Response("The value exsits in the db", status=409, mimetype="text/plain")
-   lines = read_data().splitlines()
+   replace_data(value, "")
+   return Response(f"Value {value} deleted from the db", status=200, mimetype="text/plain")
 
-   
-   
+
+@app.route("/update", methods=['PUT'])
+def update_data():
+   value = request.args.get('data')
+   new_value = request.args.get('new_data')
+   if not value:
+      return Response("No data provided", status=400, mimetype="text/plain")
+   if not data_exists(value):
+      return Response("The value exsits in the db", status=409, mimetype="text/plain")
+   replace_data(value, new_value)
+   return Response(f"Value {value} updated by {new_value}", status=200, mimetype="text/plain")
+
 if __name__ == '__main__':
    app.run(debug=True, port=8080)
