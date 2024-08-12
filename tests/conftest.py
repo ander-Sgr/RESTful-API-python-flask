7b52a3727bba0b30bb4a2351594d0ca96f453f82
ander <andersonbarreto2001@gmail.com>
2024-08-12 20:15:27 +0200

    tests done

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,25 @@
-import pytest
 import tempfile
-import os
-from server import create_app
-from FileUtils import FileUtils
+import os, csv
+import pytest
+from server import create_app, FileUtils
 
 @pytest.fixture
 def client():
     # Crear un archivo temporal para las pruebas
     with tempfile.NamedTemporaryFile(delete=False, mode='w', newline='') as tmp_file:
         file_name = tmp_file.name
+        print(f"Temporary file created at: {file_name}")
 
-    # Configurar la aplicación para usar el archivo temporal
+
+    # Configurar la aplicación Flask con el archivo temporal
     app = create_app()
-    app.config['TESTING'] = True# Configurar el archivo CSV temporal
-    app.file_utils = FileUtils(file_name)  # Inicializar FileUtils
-    try:
-        with app.test_client() as client:
-            yield client
-    finally:
-        os.remove(file_name)
+    app.config['TESTING'] = True
+    app.file_utils = FileUtils(file_name)
 
-    print(file_name)
+    with app.test_client() as client:
+        # Asegúrate de que el archivo CSV esté vacío al inicio
+        app.file_utils.create_csv_file()
+        yield client
+
+    # Eliminar el archivo temporal después de la prueba
+    os.remove(file_name)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,57 @@
 def test_insert_data(client):
-    # Primera inserción del dato
-    data = {'data': 'hello'}
-    response = client.post('/insertdata', query_string=data)
+    response = client.post('/insertdata', query_string={'data': 'test_value'})
+    assert response.status_code == 201
+    assert b"Value test_value created at the db" in response.data
     
-    # Verifica que la primera inserción fue exitosa (201 Created)
-    assert response.status_code == 201, f"Expected 201, but got {response.status_code}"
-    assert b"Value hello created at the db" in response.data
-    
+def test_insert_duplicate_data(client):
+    client.post('/insertdata', query_string={'data': 'test_value'})
+    response = client.post('/insertdata', query_string={'data': 'test_value'})
+    assert response.status_code == 409
+    assert b"The value exists in the db" in response.data
+
+def test_not_insert_nothing(client):
+    response = client.post('/insertdata')
+    assert response.status_code == 400
+    assert b"No data provided" in response.data
+
+def test_retrieve_all_data(client):
+    response = client.post('/insertdata', query_string={'data': 'test_value'})
+    response = client.post('/insertdata', query_string={'data': 'test_value2'})
+    response = client.get('/getdata')
+    assert response.status_code == 200
+    assert b"test_value" in response.data
+    assert b"test_value2" in response.data
+
+def test_no_retrieve_nothing(client):
+    response = client.get('/getdata')
+    assert response.status_code == 404
+    assert b"No data found" in response.data
+
+def test_delete_data(client):
+    response = client.post('/insertdata', query_string={'data': 'test_value'})
+    response = client.delete('/delete', query_string={'data': 'test_value'})
+    assert response.status_code == 200
+    assert b"Value test_value deleted from the db" in response.data
+    response = client.delete('/delete', query_string={'data': 'test_value'})
+
+def test_no_value_provided_for_deleting(client):
+    response = client.delete('/delete')
+    assert response.status_code == 400
+    assert b"No data provided" in response.data
+
+def test_delete_an_inexistent_value(client):
+    response = client.delete('/delete', query_string={'data': 'test_value'})
+    assert response.status_code == 409
+    assert b"The value not exists in the db" in response.data
+
+def test_update_data(client):
+    client.post('/insertdata', query_string={'data': 'hello world'})
+    response = client.put('/update', query_string={'data': 'hello world', 'new_data': 'hello universe'})
+    assert response.status_code == 200
+    assert b"Value hello world updated by hello universe"
+
+def test_update_an_inexistent_value(client):
+    response = client.put('/update', query_string={'data': 'hello world', 'new_data': 'hello universe'})
+    assert response.status_code == 409
+    assert b"The value not exists in the db"
+
